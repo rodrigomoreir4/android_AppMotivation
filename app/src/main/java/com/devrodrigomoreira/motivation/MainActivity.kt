1566ef208a9511f945a1826d5227b2c42397cb83
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-07-30 14:36:42 -0300

    View binding

--- app/src/main/java/com/devrodrigomoreira/motivation/MainActivity.kt
+++ app/src/main/java/com/devrodrigomoreira/motivation/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.devrodrigomoreira.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.devrodrigomoreira.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_new_phrase){
+            var s = ""
+        }
     }
 }
