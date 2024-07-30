2fe8286455ee656e693bad702b06373959176d18
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-07-30 15:42:58 -0300

    UserActivity

---
+++ app/src/main/java/com/devrodrigomoreira/motivation/UserActivity.kt
@@ -0,0 +1,30 @@
+package com.devrodrigomoreira.motivation
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devrodrigomoreira.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            var s = ""
+        }
+    }
+}
